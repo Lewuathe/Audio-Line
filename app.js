99bc9f14fced81976f67f9b3151647224d0a60ca
lewuathe <lewuathe@me.com>
2013-08-14 23:35:20 +0900

    Add for flat designes

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
+app.use(express.cookieParser('your secret here'));
 app.use(express.methodOverride());
 app.use(passport.initialize());
 app.use(passport.session());
