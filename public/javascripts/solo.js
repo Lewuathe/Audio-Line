814af7b03560ce7a6fae8892f703fd4fd32c7882
Lewuathe <lewuathe@me.com>
2013-08-17 14:02:32 +0900

    Add Three.js

--- public/javascripts/solo.js
+++ public/javascripts/solo.js
@@ -1,3 +1,5 @@
+var gSound;
+
 function Notes(){
     var self = this;
     self.noteList = [];
@@ -194,3 +196,49 @@ function oneliner(tweets) {
     };
 }
 
+
+
+$(function(){
+    
+	var camera, scene, renderer;
+	var geometry, material, mesh;
+	
+	function init() {
+		
+		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+		camera.position.z = 1000;
+		
+		scene = new THREE.Scene();
+		
+		geometry = new THREE.CubeGeometry( 200, 200, 200 );
+		material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+		
+		mesh = new THREE.Mesh( geometry, material );
+		scene.add( mesh );
+		
+		renderer = new THREE.CanvasRenderer();
+		renderer.setSize( 800, 500 );
+		
+		document.body.appendChild( renderer.domElement );
+		
+	}
+	
+	function animate() {
+		// note: three.js includes requestAnimationFrame shim
+		requestAnimationFrame( animate );
+		
+		mesh.rotation.x += 0.01;
+		mesh.rotation.y += 0.02;
+		
+		renderer.render( scene, camera );
+		
+	}
+	
+    
+	pico.play(oneliner(tweets));
+
+	init();
+	animate();
+
+
+});
